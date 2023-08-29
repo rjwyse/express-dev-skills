e78c9f6e3db51f2192e0d80b7bdfb725160bcd5d
rjwyse <wyse.rob27@gmail.com>
2023-08-29 15:59:56 -0400

    update4:00

--- express-dev-skills/routes/skills.js
+++ express-dev-skills/routes/skills.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var skillsCtrl = require('../controllers/skillls');
+var skillsCtrl = require('../controllers/skills');
 
 
 //All actual paths start with "/skillls"
